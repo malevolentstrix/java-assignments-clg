8721002981c9504be7766fb2ccb080afd7736157
unknown <jithintvm1@gmail.com>
2020-12-23 23:37:29 +0530

    Cleared repo

--- automate-the-boring-stuff-with-python/chapter-1/in-chapter-examples.py
+++
@@ -1,8 +0,0 @@
-print('Hello World')
-print('Whats ur name')
-myName = input()
-print('It is good to meet you'+myName)
-print(len(myName))
-print('What is your age?')
-age = input()
-print('You will be '+ str(int(age)+1) +' in a year')
